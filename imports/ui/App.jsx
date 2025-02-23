5c2184f014c9f12a0629d89cbc17e7081179efd2
GabrielFrazz <carlos.gofrazao@gmail.com>
2025-02-22 22:56:40 -0300

    Formulário de login para testes

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,14 +2,21 @@ import { Meteor } from 'meteor/meteor';
 import React, { useState, Fragment} from 'react';
 import { useTracker, useSubscribe } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
+import { LoginForm } from './pages/LoginForm';
 
 
 export const App = () => {
+  const user = useTracker(()=>Meteor.user());
+  const logout = () => Meteor.logout();
 
-  
+  if (!user) {
+    return <LoginForm />;
+  }
+
   return(
     <div>
       <h1>Inicio do projeto</h1>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 
